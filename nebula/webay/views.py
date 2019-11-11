c4b2033e3e9ac9fbbf845d07e65ae6092b0511b2
Nabilah <nabilah.990@hotmail.co.uk>
2019-11-11 07:39:06 +0000

    Revert "Revert "Profile page""
    This reverts commit 01713e7b3cc2090c9b008cb2c0f4b31c3aae369a.

--- nebula/webay/views.py
+++ nebula/webay/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'webay/index.html')
+
+
+def profile(request):
+    return render(request, 'webay/profile.html')
