01713e7b3cc2090c9b008cb2c0f4b31c3aae369a
Nabilah <nabilah.990@hotmail.co.uk>
2019-11-11 07:35:17 +0000

    Revert "Profile page"
    This reverts commit 6af15f77ff8c970a0428033fc56a71d76d780cb3.

--- nebula/webay/urls.py
+++ nebula/webay/urls.py
@@ -2,7 +2,4 @@ from django.urls import path
 from . import views
 
 app_name='webay'
-urlpatterns = [
-path('', views.index, name='index'),
-path('profile/', views.profile, name='profile'),
-]
+urlpatterns = [path('', views.index, name='index'),]
--- nebula/webay/views.py
+++ nebula/webay/views.py
@@ -3,7 +3,3 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'webay/index.html')
-
-
-def profile(request):
-    return render(request, 'webay/profile.html')
