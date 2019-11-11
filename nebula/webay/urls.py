6af15f77ff8c970a0428033fc56a71d76d780cb3
Nabilah <nabilah.990@hotmail.co.uk>
2019-11-11 07:32:32 +0000

    Profile page

--- nebula/webay/urls.py
+++ nebula/webay/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 
 app_name='webay'
-urlpatterns = [path('', views.index, name='index'),]
+urlpatterns = [
+path('', views.index, name='index'),
+path('profile/', views.profile, name='profile'),
+]
